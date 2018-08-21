65d5188d121f74ff86fbae9e059a06d08c957571
Vikas Pal <pal.vikas210@gmail.com>
2018-08-21 17:21:39 +0530

    Initial commit

---
+++ app/src/main/java/com/example/cas/launchappfromapp/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.cas.launchappfromapp;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void startNewActivity(View view) {
+
+        //package name of app you want to launch.
+        String packageName="com.google.android.youtube";
+        Context context=this;
+        Intent intent = context.getPackageManager().getLaunchIntentForPackage(packageName);
+        if (intent == null) {
+            // Bring user to the market or let them choose an app?
+            intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse("market://details?id=" + packageName));
+        }
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(intent);
+    }
+}
